1cf915ff297da4a4e4cd97b166510cc9f503c286
renzorevq@gmail.com <u201717085@upc.edu.pe>
2025-11-16 19:42:43 -0500

    feat: estructura basica inicial

---
+++ public/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btn_inicio = document.getElementById("btn_inicio");
+  const btn_crear_reporte = document.getElementById("btn_crear_reporte");
+  const btn_seguimiento = document.getElementById("btn_seguimiento");
+  const btn_comunidad = document.getElementById("btn_comunidad");
+  const btn_cuenta_soporte = document.getElementById("btn_cuenta_soporte");
+
+  btn_inicio.addEventListener("click", function () {
+    window.location.href = "/index.html";
+  });
+
+  btn_crear_reporte.addEventListener("click", function () {
+    window.location.href = "/pages/crear_reportes/crear_reporte.html";
+  });
+
+  btn_seguimiento.addEventListener("click", function () {
+    window.location.href = "/pages/seguimiento/seguimiento.html";
+  });
+
+  btn_comunidad.addEventListener("click", function () {
+    window.location.href = "/pages/comunidad/comunidad.html";
+  });
+
+  btn_cuenta_soporte.addEventListener("click", function () {
+    window.location.href = "/pages/cuenta_soporte/cuenta_soporte.html";
+  });
+});
