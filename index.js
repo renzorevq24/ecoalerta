81df40604df6b174199c1717c9131ba821d1e45c
renzorevq@gmail.com <u201717085@upc.edu.pe>
2025-11-16 22:04:13 -0500

    fix: linkeo de paginas

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const btn_cuenta_soporte = document.getElementById("btn_cuenta_soporte");
 
   btn_inicio.addEventListener("click", function () {
-    window.location.href = "/index.html";
+    window.location.href = "index.html";
   });
 
   btn_crear_reporte.addEventListener("click", function () {
-    window.location.href = "/pages/crear_reportes/crear_reporte.html";
+    window.location.href = "pages/crear_reportes/crear_reporte.html";
   });
 
   btn_seguimiento.addEventListener("click", function () {
-    window.location.href = "/pages/seguimiento/seguimiento.html";
+    window.location.href = "pages/seguimiento/seguimiento.html";
   });
 
   btn_comunidad.addEventListener("click", function () {
-    window.location.href = "/pages/comunidad/comunidad.html";
+    window.location.href = "pages/comunidad/comunidad.html";
   });
 
   btn_cuenta_soporte.addEventListener("click", function () {
-    window.location.href = "/pages/cuenta_soporte/cuenta_soporte.html";
+    window.location.href = "pages/cuenta_soporte/cuenta_soporte.html";
   });
 });
