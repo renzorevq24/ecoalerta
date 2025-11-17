e5fb6a67e37f326c7a2399da22fec4be1f5bab59
renzorevq@gmail.com <u201717085@upc.edu.pe>
2025-11-16 22:38:21 -0500

    fix: correccion rutas

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const btn_cuenta_soporte = document.getElementById("btn_cuenta_soporte");
 
   btn_inicio.addEventListener("click", function () {
-    window.location.href = "./index.html";
+    window.location.href = "/ecoalerta/index.html";
   });
 
   btn_crear_reporte.addEventListener("click", function () {
-    window.location.href = "./pages/crear_reportes/crear_reporte.html";
+    window.location.href = "/ecoalerta/pages/crear_reportes/crear_reporte.html";
   });
 
   btn_seguimiento.addEventListener("click", function () {
-    window.location.href = "./pages/seguimiento/seguimiento.html";
+    window.location.href = "/ecoalerta/pages/seguimiento/seguimiento.html";
   });
 
   btn_comunidad.addEventListener("click", function () {
-    window.location.href = "./pages/comunidad/comunidad.html";
+    window.location.href = "/ecoalerta/pages/comunidad/comunidad.html";
   });
 
   btn_cuenta_soporte.addEventListener("click", function () {
-    window.location.href = "./pages/cuenta_soporte/cuenta_soporte.html";
+    window.location.href =
+      "/ecoalerta/pages/cuenta_soporte/cuenta_soporte.html";
   });
 });
